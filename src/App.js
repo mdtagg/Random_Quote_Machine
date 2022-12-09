76ee0518b89dc5ea45c220ece7844785160248c6
Mike Tagg <mdtagg91@gmail.com>
2022-12-08 17:48:56 -0800

    quote machine base styling and stucture added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div className='quote-box'>
+          <div className='quote-container'>
+            <div id='quote'>test test test test test test test test test test test test test</div>
+            <div id='quote-author'>-author</div>
+          </div>
+          <div className='new-quote-container'>
+            <div className='socials'>
+              <button className='button'></button>
+              <button className='button'></button>
+            </div>
+            <div className='new-quote'>
+              <button className='button' id='new-button'>
+                New Quote
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    }
 
 export default App;
